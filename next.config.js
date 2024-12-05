e287f1de64bbe7ce07a352513fdb6db1aa2665db
minhyuk <kazesky93@gmail.com>
2024-12-05 13:32:39 +0900

    baseUrl 오류

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
-  basePath: '/worklog'
+  register: true,
+  skipWaiting: true
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/worklog'
+  basePath: '/worklog',
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  }
 }
 
 module.exports = withPWA(nextConfig) 
