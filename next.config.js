739f2b3f59461b9ec6f02ee5b3717bd75546a9b0
minhyuk <kazesky93@gmail.com>
2024-12-06 14:46:24 +0900

    pwa 기능 수정, 아이콘 등록

--- next.config.js
+++ next.config.js
@@ -2,18 +2,16 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
-  skipWaiting: true
+  skipWaiting: true,
+  buildExcludes: [/middleware-manifest\.json$/],
+  publicExcludes: ['!worklog/**/*'],
+  scope: '/worklog'
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/worklog',
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  }
+  basePath: '/worklog'
 }
 
 module.exports = withPWA(nextConfig) 
